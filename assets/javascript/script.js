0a06c68f1af5a6975c2c2054f2e73d831065301c
Caleb Korson <caleb@Calebs-MacBook-Air.local>
2022-08-14 09:17:22 -0400

    Added check for user input

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,9 @@
-let playerInput = prompt("Please enter R, P, or S");
-var computerInput = ["R","P","S"]
+var gameOver = false;
+var computerInput = ["R","P","S"];
 
 function getRandom() {
    return Math.floor(Math.random() * 3)
 }
-
-var x = getRandom()
-console.log("You played " + playerInput)
-console.log("The computer played " + computerInput[x]);
-alert("The computer played " + computerInput[x])
 
 function rock() {
     if (computerInput[x] === "P") {
@@ -37,20 +32,45 @@ function scissors() {
     }
 }
 
+function tryAgain() {
+    return ("Please enter R, P, or S")
+}
+
 function findWinner() {
     if (playerInput === computerInput[x]) {
+        gameOver = true
+        alert("The computer played " + computerInput[x])
         return ("It's a tie!")
     }
     else if (playerInput === "R") {
+        gameOver = true
+        alert("The computer played " + computerInput[x])
         return (rock())
     }
     else if (playerInput === "S") {
+        gameOver = true
+        alert("The computer played " + computerInput[x])
         return (scissors())
     }
     else if (playerInput === "P") {
+        gameOver = true
+        alert("The computer played " + computerInput[x])
         return (paper())
+    }
+    else {
+        return (tryAgain())
     }
 }
 
-console.log(findWinner())
-alert(findWinner())
+function playAgain() {
+    if (confirm("Would you like to play again?") = false) {
+        gameOver = true
+    }
+}
+
+while (gameOver === false) {
+    var playerInput = prompt("Please enter R, P, or S").toUpperCase();
+    var x = getRandom()
+    alert(findWinner())
+    // playAgain()
+}
